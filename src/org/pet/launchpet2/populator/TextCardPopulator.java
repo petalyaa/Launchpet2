c43cae1d19be6c5467ad20ead485730a55eda28f
Mohd Khairul Ikhwan bin Kamarudin <petalyaa@gmail.com>
2014-12-08 11:49:56 +0800

    Update text card view to more simplified version

--- src/org/pet/launchpet2/populator/TextCardPopulator.java
+++ src/org/pet/launchpet2/populator/TextCardPopulator.java
@@ -1,14 +1,15 @@
 package org.pet.launchpet2.populator;
 
+import org.jsoup.Jsoup;
 import org.pet.launchpet2.R;
 import org.pet.launchpet2.model.HomeNewsItem;
-import org.pet.launchpet2.util.ConfigurationUtil;
+import org.pet.launchpet2.thread.FetchFaviconAsync;
 
 import android.annotation.SuppressLint;
-import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class TextCardPopulator extends Populator {
@@ -23,14 +24,17 @@ public class TextCardPopulator extends Populator {
 		View card = inflater.inflate(R.layout.home_text_card, null);
 		ImageView imageView = (ImageView) card.findViewById(R.id.card_text_icon_image);
 		TextView titleTextView = (TextView) card.findViewById(R.id.card_text_title_label);
-		TextView dateTextView = (TextView) card.findViewById(R.id.card_text_date_label);
 		TextView contentTextView = (TextView) card.findViewById(R.id.card_text_content_label);
-		if(item.getIcon() != null)
-			imageView.setImageBitmap(item.getIcon());
+		RelativeLayout titleHolder = (RelativeLayout) card.findViewById(R.id.card_text_title_holder);
+		int titleBackgroundColor = getTitleBackgroundColor();
+		if(titleBackgroundColor != 0)
+			titleHolder.setBackgroundColor(titleBackgroundColor);
+		imageView.setImageResource(R.drawable.rss);
+		new FetchFaviconAsync(imageView).execute(item.get("parentLink"));
 		titleTextView.setText(item.getTitle());
-		String text = Html.fromHtml(item.getContent()).toString();
-		contentTextView.setText(text);
-		dateTextView.setText(ConfigurationUtil.NEWS_FEED_DATE_FORMAT.format(item.getDate()));
+		String text = item.getContent();
+		text = Jsoup.parse(text).text();
+		contentTextView.setText("\"" + text + "…\"");
 		return card;
 	}
 
