ec6d68903974905a8f5f91a5d5e1a43b5c3f15d1
Mohd Khairul Ikhwan bin Kamarudin <petalyaa@gmail.com>
2014-12-14 04:59:27 +0800

    Added utility for converting string to camel case

--- src/org/pet/launchpet2/util/StringUtil.java
+++ src/org/pet/launchpet2/util/StringUtil.java
@@ -1,17 +1,33 @@
 package org.pet.launchpet2.util;
 
+import android.annotation.SuppressLint;
+
 public class StringUtil {
 
 	public static final String shortened(String s, int length) {
-		if(s.length() > length) {
+		if (s.length() > length) {
 			s = s.substring(0, length);
 			s = s + "…";
 		}
 		return s;
 	}
-	
+
 	public static final boolean isNullEmptyString(String str) {
 		return str == null || str.equals("");
 	}
-	
+
+	public static final String toCamelCase(String s) {
+		String[] parts = s.split(" ");
+		String camelCaseString = "";
+		for (String part : parts) {
+			camelCaseString = camelCaseString + " " + toProperCase(part);
+		}
+		return camelCaseString;
+	}
+
+	@SuppressLint("DefaultLocale")
+	public static final String toProperCase(String s) {
+		return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
+	}
+
 }
