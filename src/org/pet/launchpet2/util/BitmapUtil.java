b069905f7dba2d3d01c47fba89414e8204e8a83a
Mohd Khairul Ikhwan bin Kamarudin <petalyaa@gmail.com>
2014-12-15 09:43:56 +0800

    Added memcache to image loading from url

--- src/org/pet/launchpet2/util/BitmapUtil.java
+++ src/org/pet/launchpet2/util/BitmapUtil.java
@@ -35,6 +35,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore.Images;
+import android.util.Log;
 import android.util.LruCache;
 import android.util.TypedValue;
 import android.view.Display;
@@ -42,22 +43,26 @@ import android.view.View;
 import android.view.View.MeasureSpec;
 
 public class BitmapUtil {
-	
-	private static final int MAX_MEMORY = (int) (Runtime.getRuntime().maxMemory() / 1024);
 
-    private static final int CACHE_SIZE = MAX_MEMORY / 8;
-	
-	private static final LruCache<String, Bitmap> MEMORY_CACHE = new LruCache<String, Bitmap>(CACHE_SIZE) {
-		
-        @Override
-        protected int sizeOf(String key, Bitmap bitmap) {
-            return bitmap.getByteCount() / 1024;
-        }
-        
-    };
+	private static final int MAX_MEMORY = (int) (Runtime.getRuntime()
+			.maxMemory() / 1024);
+
+	private static final int CACHE_SIZE = MAX_MEMORY / 8;
+
+	private static final LruCache<String, Bitmap> MEMORY_CACHE = new LruCache<String, Bitmap>(
+			CACHE_SIZE) {
+
+		@Override
+		protected int sizeOf(String key, Bitmap bitmap) {
+			return bitmap.getByteCount() / 1024;
+		}
+
+	};
 
 	static enum BitmapType {
-		FAVICON(ConfigurationUtil.SUBDIRECTORY_FAVICON), IMAGES(ConfigurationUtil.SUBDIRECTORY_IMAGES);
+		FAVICON(ConfigurationUtil.SUBDIRECTORY_FAVICON), IMAGES(
+				ConfigurationUtil.SUBDIRECTORY_IMAGES), APP_ICONS(
+				ConfigurationUtil.SUBDIRECTORY_APP_ICONS);
 
 		private BitmapType(String s) {
 			this.s = s;
@@ -69,48 +74,53 @@ public class BitmapUtil {
 
 		private String s;
 	};
-	
+
 	public static final void addBitmapToMemoryCache(String key, Bitmap bitmap) {
-	    if (getBitmapFromMemCache(key) == null) {
-	    	MEMORY_CACHE.put(key, bitmap);
-	    }
+		if (getBitmapFromMemCache(key) == null) {
+			MEMORY_CACHE.put(key, bitmap);
+		}
 	}
-	
+
 	public static final Bitmap getBitmapFromMemCache(String key) {
-	    return MEMORY_CACHE.get(key);
+		return MEMORY_CACHE.get(key);
 	}
 
 	public static final Bitmap getCurrentScreenShot(View view) {
-		Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Config.ARGB_8888);
+		Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),
+				Config.ARGB_8888);
 		Canvas canvas = new Canvas(bitmap);
 		view.draw(canvas);
 		return bitmap;
 	}
 
-	public static final Bitmap getBitmapFromView(View view, int height, int width) {
-//		int specWidth = MeasureSpec.makeMeasureSpec(parentWidth, MeasureSpec.AT_MOST);
-//		Bitmap b = Bitmap.createBitmap(v.getLayoutParams().width, v.getLayoutParams().height, Bitmap.Config.ARGB_8888);                
-//		Canvas c = new Canvas(b);
-//		v.layout(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
-//		v.draw(c);
-//		return b;
-		view.measure(View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY),
-	            View.MeasureSpec.makeMeasureSpec(height, View.MeasureSpec.EXACTLY));
-	    view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+	public static final Bitmap getBitmapFromView(View view, int height,
+			int width) {
+		// int specWidth = MeasureSpec.makeMeasureSpec(parentWidth,
+		// MeasureSpec.AT_MOST);
+		// Bitmap b = Bitmap.createBitmap(v.getLayoutParams().width,
+		// v.getLayoutParams().height, Bitmap.Config.ARGB_8888);
+		// Canvas c = new Canvas(b);
+		// v.layout(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
+		// v.draw(c);
+		// return b;
+		view.measure(View.MeasureSpec.makeMeasureSpec(width,
+				View.MeasureSpec.EXACTLY), View.MeasureSpec.makeMeasureSpec(
+				height, View.MeasureSpec.EXACTLY));
+		view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
 
-	    // Build the Drawing Cache
-	    view.buildDrawingCache();
+		// Build the Drawing Cache
+		view.buildDrawingCache();
 
-	    // Create Bitmap
-	    Bitmap drawingCache = view.getDrawingCache();
-	    if (drawingCache == null) {
-	        return null;
-	    }
+		// Create Bitmap
+		Bitmap drawingCache = view.getDrawingCache();
+		if (drawingCache == null) {
+			return null;
+		}
 
-	    Bitmap bitmap = Bitmap.createBitmap(drawingCache);
-	    drawingCache.recycle();
-	    view.setDrawingCacheEnabled(false);
-	    return bitmap;
+		Bitmap bitmap = Bitmap.createBitmap(drawingCache);
+		drawingCache.recycle();
+		view.setDrawingCacheEnabled(false);
+		return bitmap;
 	}
 
 	public static final Bitmap getBitmapFromResource(Context context, int resId) {
@@ -119,12 +129,13 @@ public class BitmapUtil {
 
 	public static final Bitmap createStackBitmapFromList(List<Bitmap> bitmapList) {
 		Bitmap bitmap = null;
-		if(bitmapList != null && bitmapList.size() > 0) {
-			for(Bitmap tmpBitmap : bitmapList) {
-				if(bitmap == null) {
+		if (bitmapList != null && bitmapList.size() > 0) {
+			for (Bitmap tmpBitmap : bitmapList) {
+				if (bitmap == null) {
 					bitmap = tmpBitmap;
 				} else {
-					Bitmap mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+					Bitmap mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888,
+							true);
 					Canvas canvas = new Canvas(mutableBitmap);
 					Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG);
 					canvas.drawBitmap(tmpBitmap, 0, 0, paint);
@@ -138,49 +149,111 @@ public class BitmapUtil {
 		if (drawable instanceof BitmapDrawable) {
 			return ((BitmapDrawable) drawable).getBitmap();
 		}
-		Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Config.ARGB_8888);
-		Canvas canvas = new Canvas(bitmap); 
+		Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+				drawable.getIntrinsicHeight(), Config.ARGB_8888);
+		Canvas canvas = new Canvas(bitmap);
 		drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
 		drawable.draw(canvas);
 		return bitmap;
 	}
 
-	public static final Bitmap getBitmapFromPackage(Context context, String packageName) {
-		Bitmap bitmap = getBitmapFromMemCache(packageName);
-		if(bitmap != null)
+	public static final Bitmap getBitmapFromPackage(Context context,
+			String packageName) {
+		// Bitmap bitmap = getBitmapFromMemCache(packageName);
+		Bitmap bitmap = getBitmapFromCache(packageName, BitmapType.APP_ICONS);
+		if (bitmap != null) {
 			return bitmap;
+		}
 		Drawable drawable = null;
 		PackageManager pm = context.getPackageManager();
 		Intent i = new Intent(Intent.ACTION_MAIN, null);
 		i.addCategory(Intent.CATEGORY_LAUNCHER);
-		//get a list of installed apps.
+		// get a list of installed apps.
 		List<ResolveInfo> availableActivities = pm.queryIntentActivities(i, 0);
-		for(ResolveInfo ri:availableActivities){
+		for (ResolveInfo ri : availableActivities) {
 			LauncherApplication app = new LauncherApplication();
 			String thisPackageName = ri.activityInfo.packageName;
-			if(packageName.equals(thisPackageName)) {
+			if (packageName.equals(thisPackageName)) {
 				drawable = ri.activityInfo.loadIcon(pm);
 			}
 			app.setIcon(ri.activityInfo.loadIcon(pm));
 		}
 		bitmap = getBitmapFromDrawable(drawable);
-		addBitmapToMemoryCache(packageName, bitmap);
+		// addBitmapToMemoryCache(packageName, bitmap);
+		writeBitmapToFile(bitmap, getAppDrawerIconCacheFile(packageName));
 		return bitmap;
 	}
 
+	private static final File getAppDrawerIconCacheFile(String packageName) {
+		String root = Environment.getExternalStorageDirectory().toString();
+		File myDir = new File(root + "/"
+				+ ConfigurationUtil.APPLICATION_SD_DIRECTORY);
+		if (!myDir.exists())
+			myDir.mkdir();
+		myDir = new File(myDir, ConfigurationUtil.SUBDIRECTORY_CACHE);
+		if (!myDir.exists())
+			myDir.mkdir();
+		myDir = new File(myDir, ConfigurationUtil.SUBDIRECTORY_APP_ICONS);
+		if (!myDir.exists())
+			myDir.mkdir();
+		String fileName = constructUniqueName(packageName);
+		return new File(myDir, fileName);
+	}
+
 	public static final Bitmap getFavicon(String urlStr) {
-		if(urlStr.startsWith("http://9gag.com/")) // Hardcoded for 9gag
+		if (urlStr.startsWith("http://9gag.com/")) // Hardcoded for 9gag
 			urlStr = "http://9gag.com/";
-		if(urlStr.contains("cnn.com/")) // Hardcoded for cnn
+		if (urlStr.contains("cnn.com/")) // Hardcoded for cnn
 			urlStr = "http://edition.cnn.com/";
-		if(!urlStr.endsWith("/"))
+		if (!urlStr.endsWith("/"))
 			urlStr = urlStr + "/";
 		urlStr = urlStr + "favicon.ico";
 		return getBitmapFromCache(urlStr, BitmapType.FAVICON);
 	}
 
 	public static final Bitmap getFeedImageContent(String urlStr) {
+//		String key = constructUniqueName(urlStr);
+//		Bitmap bitmap = getBitmapFromMemCache(key);
+//		if (bitmap != null)
+//			return bitmap;
+//		bitmap = getBitmapFromURL(urlStr);
+//		addBitmapToMemoryCache(key, bitmap);
+//		return bitmap;
 		return getBitmapFromCache(urlStr, BitmapType.IMAGES);
+	}
+
+	public static final Bitmap decodeSampledBitmapFromFile(String path, int reqWidth, int reqHeight) { // BEST QUALITY MATCH
+
+		// First decode with inJustDecodeBounds=true to check dimensions
+		final BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(path, options);
+
+		// Calculate inSampleSize
+		// Raw height and width of image
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		options.inPreferredConfig = Bitmap.Config.RGB_565;
+		int inSampleSize = 1;
+
+		if (height > reqHeight) {
+			inSampleSize = Math.round((float) height / (float) reqHeight);
+		}
+
+		int expectedWidth = width / inSampleSize;
+
+		if (expectedWidth > reqWidth) {
+			// if(Math.round((float)width / (float)reqWidth) > inSampleSize) //
+			// If bigger SampSize..
+			inSampleSize = Math.round((float) width / (float) reqWidth);
+		}
+
+		options.inSampleSize = inSampleSize;
+
+		// Decode bitmap with inSampleSize set
+		options.inJustDecodeBounds = false;
+
+		return BitmapFactory.decodeFile(path, options);
 	}
 
 	public static final String constructUniqueName(String url) {
@@ -190,29 +263,35 @@ public class BitmapUtil {
 	public static final Uri getImageUri(Context inContext, Bitmap inImage) {
 		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
 		inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-		String path = Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+		String path = Images.Media.insertImage(inContext.getContentResolver(),
+				inImage, "Title", null);
 		return Uri.parse(path);
 	}
 
 	public static final Bitmap getBitmapFromCache(String urlStr, BitmapType type) {
 		Bitmap bmp = null;
 		String root = Environment.getExternalStorageDirectory().toString();
-		File myDir = new File(root + "/" + ConfigurationUtil.APPLICATION_SD_DIRECTORY);
-		if(!myDir.exists())
+		File myDir = new File(root + "/"
+				+ ConfigurationUtil.APPLICATION_SD_DIRECTORY);
+		if (!myDir.exists())
 			myDir.mkdir();
 		myDir = new File(myDir, ConfigurationUtil.SUBDIRECTORY_CACHE);
-		if(!myDir.exists())
+		if (!myDir.exists())
 			myDir.mkdir();
 		myDir = new File(myDir, type.getSubdirectory());
-		if(!myDir.exists())
+		if (!myDir.exists())
 			myDir.mkdir();
 		String fileName = constructUniqueName(urlStr);
 		myDir = new File(myDir, fileName);
-		if(myDir.exists()) {
+		if (myDir.exists()) {
 			// Bitmap file exist in cache. No need to get from internet
 			bmp = getBitmapFromFile(myDir);
 		} else {
-			// Bitmap file not exist in cache. No other choice, have to fetch from the net... :(
+			if (type == BitmapType.APP_ICONS) {
+				return null;
+			}
+			// Bitmap file not exist in cache. No other choice, have to fetch
+			// from the net... :(
 			bmp = getBitmapFromURL(urlStr);
 			// Then we store it for future use... :)
 			writeBitmapToFile(bmp, myDir);
@@ -240,10 +319,11 @@ public class BitmapUtil {
 
 	public static final Bitmap getBitmapFromURL(String urlStr) {
 		Bitmap bmp = null;
-		if(urlStr != null && !urlStr.equals("")) {
+		if (urlStr != null && !urlStr.equals("")) {
 			try {
 				URL url = new URL(urlStr);
-				bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+				bmp = BitmapFactory.decodeStream(url.openConnection()
+						.getInputStream());
 			} catch (MalformedURLException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
@@ -256,16 +336,20 @@ public class BitmapUtil {
 	public static final Bitmap getBitmapFromFile(File file) {
 		BitmapFactory.Options options = new BitmapFactory.Options();
 		options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-		Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+		Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(),
+				options);
 		return bitmap;
 	}
 
-	public static final Bitmap decodeUriAndResize(Uri selectedImage, ContentResolver contentResolver, int size) throws FileNotFoundException {
+	public static final Bitmap decodeUriAndResize(Uri selectedImage,
+			ContentResolver contentResolver, int size)
+			throws FileNotFoundException {
 
 		// Decode image size
 		BitmapFactory.Options o = new BitmapFactory.Options();
 		o.inJustDecodeBounds = true;
-		BitmapFactory.decodeStream(contentResolver.openInputStream(selectedImage), null, o);
+		BitmapFactory.decodeStream(
+				contentResolver.openInputStream(selectedImage), null, o);
 
 		// The new size we want to scale to
 		final int REQUIRED_SIZE = size;
@@ -274,8 +358,7 @@ public class BitmapUtil {
 		int width_tmp = o.outWidth, height_tmp = o.outHeight;
 		int scale = 1;
 		while (true) {
-			if (width_tmp / 2 < REQUIRED_SIZE
-					|| height_tmp / 2 < REQUIRED_SIZE) {
+			if (width_tmp / 2 < REQUIRED_SIZE || height_tmp / 2 < REQUIRED_SIZE) {
 				break;
 			}
 			width_tmp /= 2;
@@ -286,41 +369,48 @@ public class BitmapUtil {
 		// Decode with inSampleSize
 		BitmapFactory.Options o2 = new BitmapFactory.Options();
 		o2.inSampleSize = scale;
-		return BitmapFactory.decodeStream(contentResolver.openInputStream(selectedImage), null, o2);
-	}
-	
-	public static final Bitmap getRoundedCornerBitmap(Bitmap bitmap, int color, int border, int corner, Context context) {
-	    Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(),
-	            Bitmap.Config.ARGB_8888);
-	    Canvas canvas = new Canvas(output);
-
-	    final int borderSizePx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (float) border, context.getResources().getDisplayMetrics());
-	    final int cornerSizePx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (float) corner, context.getResources().getDisplayMetrics());
-	    final Paint paint = new Paint();
-	    final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
-	    final RectF rectF = new RectF(rect);
-
-	    // prepare canvas for transfer
-	    paint.setAntiAlias(true);
-	    paint.setColor(0xFFFFFFFF);
-	    paint.setStyle(Paint.Style.FILL);
-	    canvas.drawARGB(0, 0, 0, 0);
-	    canvas.drawRoundRect(rectF, cornerSizePx, cornerSizePx, paint);
-
-	    // draw bitmap
-	    paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-	    canvas.drawBitmap(bitmap, rect, rect, paint);
-
-	    // draw border
-	    paint.setColor(color);
-	    paint.setStyle(Paint.Style.STROKE);
-	    paint.setStrokeWidth((float) borderSizePx);
-	    canvas.drawRoundRect(rectF, cornerSizePx, cornerSizePx, paint);
-
-	    return output;
+		return BitmapFactory.decodeStream(
+				contentResolver.openInputStream(selectedImage), null, o2);
 	}
 
-	public static final Bitmap getCircularBitmapWithWhiteBorder(Bitmap bitmap, int borderWidth, int borderColor) {
+	public static final Bitmap getRoundedCornerBitmap(Bitmap bitmap, int color,
+			int border, int corner, Context context) {
+		Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+				bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+		Canvas canvas = new Canvas(output);
+
+		final int borderSizePx = (int) TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_DIP, (float) border, context
+						.getResources().getDisplayMetrics());
+		final int cornerSizePx = (int) TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_DIP, (float) corner, context
+						.getResources().getDisplayMetrics());
+		final Paint paint = new Paint();
+		final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+		final RectF rectF = new RectF(rect);
+
+		// prepare canvas for transfer
+		paint.setAntiAlias(true);
+		paint.setColor(0xFFFFFFFF);
+		paint.setStyle(Paint.Style.FILL);
+		canvas.drawARGB(0, 0, 0, 0);
+		canvas.drawRoundRect(rectF, cornerSizePx, cornerSizePx, paint);
+
+		// draw bitmap
+		paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+		canvas.drawBitmap(bitmap, rect, rect, paint);
+
+		// draw border
+		paint.setColor(color);
+		paint.setStyle(Paint.Style.STROKE);
+		paint.setStrokeWidth((float) borderSizePx);
+		canvas.drawRoundRect(rectF, cornerSizePx, cornerSizePx, paint);
+
+		return output;
+	}
+
+	public static final Bitmap getCircularBitmapWithWhiteBorder(Bitmap bitmap,
+			int borderWidth, int borderColor) {
 		if (bitmap == null || bitmap.isRecycled()) {
 			return null;
 		}
@@ -328,20 +418,24 @@ public class BitmapUtil {
 		final int width = bitmap.getWidth() + borderWidth;
 		final int height = bitmap.getHeight() + borderWidth;
 
-		Bitmap canvasBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-		BitmapShader shader = new BitmapShader(bitmap, TileMode.CLAMP, TileMode.CLAMP);
+		Bitmap canvasBitmap = Bitmap.createBitmap(width, height,
+				Bitmap.Config.ARGB_8888);
+		BitmapShader shader = new BitmapShader(bitmap, TileMode.CLAMP,
+				TileMode.CLAMP);
 		Paint paint = new Paint();
 		paint.setAntiAlias(true);
 		paint.setShader(shader);
 
 		Canvas canvas = new Canvas(canvasBitmap);
-		float radius = width > height ? ((float) height) / 2f : ((float) width) / 2f;
+		float radius = width > height ? ((float) height) / 2f
+				: ((float) width) / 2f;
 		canvas.drawCircle(width / 2, height / 2, radius, paint);
 		paint.setShader(null);
 		paint.setStyle(Paint.Style.STROKE);
 		paint.setColor(borderColor);
 		paint.setStrokeWidth(borderWidth);
-		canvas.drawCircle(width / 2, height / 2, radius - borderWidth / 2, paint);
+		canvas.drawCircle(width / 2, height / 2, radius - borderWidth / 2,
+				paint);
 		return canvasBitmap;
 	}
 
